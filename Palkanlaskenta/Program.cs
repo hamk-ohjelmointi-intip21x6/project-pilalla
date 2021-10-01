5e5b37016db5738bd3f95d7c3d21a4a9c1bf20a5
Leevi Rajala <leevi20001@student.hamk.fi>
2021-10-01 08:35:06 +0300

    työntekijä luokka + listaan lisääminen aloitettu

---
+++ Palkanlaskenta/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Palkanlaskenta
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			List<Tyontekija> tyontekijat = new List<Tyontekija>();
+
+			bool quit = false;
+			while (quit == false)
+			{
+				Console.Clear();
+				Console.WriteLine("0. lopeta\n1. Työntekijälista\n2. Lisää työntekijä");
+
+				switch (Console.ReadLine())
+				{
+					case "0":
+						quit = true;
+						break;
+
+					case "1":
+						Console.Clear();
+
+						if (tyontekijat.Count > 0)
+						{
+							foreach (Tyontekija t in tyontekijat)
+							{
+								Console.WriteLine(t.Nimi);
+							}
+						} else
+						{
+							
+							Console.WriteLine("Listassa ei ole työntekijöitä.");
+							
+						}
+						Console.ReadLine();
+
+						break;
+
+					case "2":
+						Console.Clear();
+
+						Console.WriteLine("Etunimi: ");
+						string enimi = Console.ReadLine();
+
+						Console.WriteLine("Sukunimi: ");
+						string snimi = Console.ReadLine();
+
+						Tyontekija uusi = new Tyontekija(enimi, snimi);
+						tyontekijat.Add(uusi);
+						break;
+
+					default:
+						break;
+				}
+			}
+			
+
+		}
+	}
+}
---
+++ Palkanlaskenta/Tyontekija.cs
@@ -0,0 +1,22 @@
+﻿namespace Palkanlaskenta
+{
+	internal class Tyontekija
+	{
+		string Etunimi;
+		string Sukunimi;
+
+		//constructor
+		public Tyontekija(string enimi, string snimi) {
+			this.Etunimi = enimi;
+			this.Sukunimi = snimi;
+		}
+
+		public string Nimi
+		{
+			get
+			{
+				return $"{this.Etunimi} {this.Sukunimi}";
+			}
+		}
+	}
+}
